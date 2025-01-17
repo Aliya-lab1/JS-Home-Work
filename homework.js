23a4dfdff4ebe880cf9cc08dab257627f1117bcd
Aliya-lab1 <abylgazievaaliia@gmail.com>
2025-01-17 10:21:20 +0100

    Домашнее задание по типам и преобразованиям

--- homework.js
+++ homework.js
@@ -1,5 +1,22 @@
-let userName=prompt("Введите ваше имя")
+//let userName=prompt("Введите ваше имя")
 
-let greeting= "Привет";
+//let greeting= "Привет";
 
-console.log(`${greeting}, ${userName}!`);
+//console.log(`${greeting}, ${userName}!`);
+
+
+// Задание на типы данных и преобразование 
+
+let toy = prompt("Введите название игрушки, которую хотите купить:");
+let price = prompt("Введите цену игрушки (в сомах):");
+let count = prompt("Введите желаемое количество игрушек:");
+
+
+price = Number(price);
+count = Number(count);
+
+
+let totalCost = price * count;
+
+
+console.log(`Итоговая стоимость покупки: ${totalCost} сомов`);
